35c0acebe593a0d40bfe4ec16005f5d4cdc6527e
VictorXRivera <victorxrivera.professional@gmail.com>
2020-11-30 17:09:46 -0500

    Omitted an namespace

--- 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
+++ 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 
-class Program
+namespace _4_print_integer
 {
-	static void Main(string[] args)
+	class Program
 	{
-		int number = 972;
-		Console.WriteLine("{0} Mission Street", number);
+		static void Main(string[] args)
+		{
+			int number = 972;
+			Console.WriteLine("{0} Mission Street", number);
+		}
 	}
 }
