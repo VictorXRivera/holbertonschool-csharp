9a43027e9e04c59ce8ea985bc416dc627bf5ba92
VictorXRivera <victorxrivera.professional@gmail.com>
2020-11-30 18:41:31 -0500

    9-print_specifiers files

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+        Console.WriteLine("Percent: " + percent.ToString("P"));
+		Console.WriteLine("Currency: " + currency.ToString("c2"));
+	}
+}
