2cc23df237273ffa749f4c984a6c2145ca94bec3
VictorXRivera <victorxrivera.professional@gmail.com>
2020-11-30 17:43:04 -0500

    Task 6 files

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.WriteLine(string.Concat(Enumerable.Repeat(str, 3)) + "\n" + str.Substring(0, 9));
+    }
+}
