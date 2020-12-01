acf2e5d0bd0b6e624eeb5f83418ba0a3fc254452
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-01 16:33:11 -0500

    Task 11 files

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length > 0)
+        {
+            for (int space = 0; space < length; space++)
+            {
+                Console.WriteLine("{0}{1}$", new String(' ', space), length > 0 ? "\\" : "");
+            }
+        }
+        Console.WriteLine();
+    }
+}
