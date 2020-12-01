d83fa50c0bac3a22d34271eaa1e5e862d1bf2fb7
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-01 11:36:50 -0500

    Task 2 files

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class _2_print_alphabet
+    {
+        public static void Main()
+        {
+            char alphabet = 'a';
+            while (alphabet <= 'z')
+            {
+                Console.Write(alphabet);
+                alphabet++;
+            }
+        }
+    }
+}
