782a9622e1c1592742f56dc323b1a2bd88b916f9
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-01 14:48:40 -0500

    Task 8 files

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int n = Math.Abs(number % 10);
+        Console.Write(n);
+        return n;
+    }
+}
