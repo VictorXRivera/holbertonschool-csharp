9c2a1dad90e8983840d3272121d5bf07ec020042
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-01 14:06:45 -0500

    Task 6 files

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int h = 0; h < 9; h++)
+            {
+                for (int b = h + 1; b < 10; b++)
+                {
+                    Console.Write("{0}{1}{2}", h, b, (h < 8 ? ", ": ""));
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
