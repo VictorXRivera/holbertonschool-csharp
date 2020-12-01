2596b0e2c1fc11343f5c81adc780d51b0e0cd691
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-01 13:12:05 -0500

    Task 5 files

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            for (num = 0; num <= 99; num++)
+            {
+                if (num != 99)
+                {
+                Console.Write(num.ToString("00"));
+                Console.Write(", ");
+                }
+                else
+                {
+                    Console.Write("{0}\n", num);
+                }
+            }
+        }
+    }
+}
