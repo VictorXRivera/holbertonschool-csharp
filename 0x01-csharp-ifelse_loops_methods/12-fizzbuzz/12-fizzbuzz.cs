7fbc27c5d5f72b660529fb57275668f9cad75592
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-01 16:49:06 -0500

    Fizzbuzz!

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Fizzbuzz
+{
+    static void Main()
+    {
+        for (int num = 1; num <= 100; num++)
+        {
+            if (num % 15 == 0)
+            {
+                Console.Write("Fizzbuzz ");
+            }
+            else if (num % 5 == 0)
+            {
+                Console.Write("Buzz ");
+            }
+            else if (num % 3 == 0)
+            {
+                Console.Write("Fizz ");
+            }
+            else
+            {
+                Console.Write("{0} ", num);
+            }
+        }
+        Console.WriteLine();
+    }
+}
