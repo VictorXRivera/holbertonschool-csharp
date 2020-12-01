9cbae84b57ba8d709e1bf6608c73797254bba5c9
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-01 12:45:45 -0500

    Task 4 files

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            for (num = 0; num <= 98; num++)
+            {
+                Console.Write("{0} = 0x{0:x}\n", num);
+            }
+        }
+    }
+}
