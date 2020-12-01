88deab7792b72959dbebe86376af87be1f014320
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-01 11:52:46 -0500

    Task 3 files

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char alphabet;
+            for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+            {
+                if (alphabet != 'e' && alphabet != 'q')
+                Console.Write(alphabet);
+            }
+        }
+    }
+}
