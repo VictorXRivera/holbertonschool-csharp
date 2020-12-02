1476d19678e0a8867b12df1bf05a8cfa18d26610
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-02 12:56:42 -0500

    Fixed string to print if index is out of range

--- 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -7,7 +7,7 @@ class Array
         if (index > array.Length || index < 0)
         {
             Console.WriteLine("Index out of range");
-            return -1;
+            return (-1);
         }
         return array[index];
     }
