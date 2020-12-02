a0bd3e93153f05d4f773311c16a06f89ff61b6bf
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-02 14:48:49 -0500

    Task 3 files

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+        for (int start = array.Length - 1; start >= 0; start--)
+        {
+            if (start == 0)
+            {
+                Console.WriteLine(array[start]);
+                break;
+            }
+            Console.Write(array[start] + " ");
+        }
+    }
+}
