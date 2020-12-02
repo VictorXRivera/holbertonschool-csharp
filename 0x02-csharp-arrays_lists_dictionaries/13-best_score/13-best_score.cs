9e3bfae1c21397487555d88bb4dfe0cb65014549
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-02 18:18:41 -0500

    Task 13 files

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            return "None";
+        }
+        var max = myList.Aggregate((k, v) => k.Value > v.Value ? k : v).Key;
+        return max;
+    }
+}
