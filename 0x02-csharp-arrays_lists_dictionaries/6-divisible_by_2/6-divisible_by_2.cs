57c945377adc0787b5aebd93217fd307890961d8
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-02 15:59:28 -0500

    Task 6 files

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> new_list = new List<bool>();
+        foreach (int element in myList)
+        {
+            if (element % 2 == 0)
+            {
+                new_list.Add(true);
+            }
+            else
+            {
+                new_list.Add(false);
+            }
+        }
+        return new_list;
+    }
+}
