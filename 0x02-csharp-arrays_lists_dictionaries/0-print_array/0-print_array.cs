61c897935969e49206e4a07594b27ff779bda281
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-02 11:28:45 -0500

    Task 0 files

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        if (size == 0)
+        {
+            Console.WriteLine();
+        }
+        int[] arrays = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            if (i == size - 1)
+            {
+                Console.WriteLine(i);
+                arrays[i] = i;
+            }
+            else
+            {
+                Console.Write("{0} ", i);
+                arrays[i] = i;
+            }
+        }
+        return arrays;
+    }
+}
