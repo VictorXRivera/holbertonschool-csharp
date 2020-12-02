82210269cfdd222bd4f9ac264cbbbb76b022d67b
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-02 17:00:22 -0500

    Task 8 file

--- 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -5,6 +5,8 @@ class Dictionary
 {
     public static int NumberOfKeys(Dictionary<string, string> myDict)
     {
+        Dictionary<string, string>.KeyCollection my_dict = myDict.Keys;
+        
         int i = 0;
         foreach (var key in myDict)
         {
