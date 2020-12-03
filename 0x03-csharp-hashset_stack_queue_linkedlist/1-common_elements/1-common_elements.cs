724cbfdce8d689480121a56542ce44afbe84bbbc
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-03 12:30:11 -0500

    Common elements method

--- 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -6,11 +6,10 @@ class List
     public static List<int> CommonElements(List<int> list1, List<int> list2)
     {
         HashSet<int> l1 = new HashSet<int>(list1);
-
-        l1.SymmetricExceptWith(new HashSet<int>(list2));
-        
+        HashSet<int> l2 = new HashSet<int>(list2); 
         List<int> s_list = new List<int>();
 
+        l1.IntersectWith(l2);
         foreach (int num in l1)
         {
             s_list.Add(num);
