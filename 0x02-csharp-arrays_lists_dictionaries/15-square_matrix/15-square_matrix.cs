9b360ff33cab2be513e256891afe261abaf2e3d8
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-02 19:07:46 -0500

    Square Matrix program

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int row = myMatrix.GetLength(0);
+        int column = myMatrix.GetLength(1);
+
+        int[,] sq_value = new int[row, column];
+        for (int h = 0; h < row; h++)
+        {
+            for (int b = 0; b < column; b++)
+            {
+                sq_value[h, b] = myMatrix[h, b] * myMatrix[h, b];
+            }
+        }
+        return sq_value;
+    }
+}
