d5c998a01af6de8041b31e6f5cebac08feb1878c
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-03 16:13:25 -0500

    Task 4 project files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+
+        if (aQueue.Count <= 0)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        else
+        {
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        }
+
+        bool value = true;
+        value = (aQueue.Contains(search));
+        Console.WriteLine($"Queue contains \"{search}\": {value}");
+
+        if (value)
+        {
+            while (aQueue.Dequeue() != search);
+        }
+        aQueue.Enqueue(newItem);
+        return aQueue;
+    }
+}
