b7720d3b56b4a83a670fe70b4fe81805a162ad7b
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-03 23:49:17 -0500

    Pop a node program

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int top_node;
+        if (myLList.Count == 0)
+            return 0;
+        top_node = myLList.First.Value;
+        myLList.RemoveFirst();
+        return top_node;
+    }
+}
