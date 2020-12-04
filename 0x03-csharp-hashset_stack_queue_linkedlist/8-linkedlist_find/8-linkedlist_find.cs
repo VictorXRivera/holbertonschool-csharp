4ce142435b2d4e8e8bfcd3df74783154dfc4927e
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-03 23:39:58 -0500

    Finding node program

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int counter = 0;
+        foreach (int num in myLList)
+        {
+            if (num == value)
+            return counter;
+            counter++;
+        }
+        return -1;
+    }
+}
