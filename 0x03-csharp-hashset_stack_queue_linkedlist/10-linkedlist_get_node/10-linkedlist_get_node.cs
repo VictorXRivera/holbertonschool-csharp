734fbd286ba2b89705e5700c83cbb4ad3dc44a88
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-03 23:55:15 -0500

    Getting the node program

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int i = 0;
+        foreach (int value in myLList)
+        {
+            if (i == n)
+            {
+                return (value);
+            }
+            i++;
+        }
+        return 0;
+    }
+}
