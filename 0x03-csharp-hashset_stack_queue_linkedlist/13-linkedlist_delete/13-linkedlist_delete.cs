fc9bcb5997d770192a46d95828fefc79f6cb5b5c
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-04 00:22:48 -0500

    Delete node program

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        var node = myLList.First;
+        int i;
+
+        for (i = 0; i < index && i < myLList.Count; i++)
+        {
+            node = node.Next;
+        }
+        if (i == index)
+            myLList.Remove(node);
+    }
+}
