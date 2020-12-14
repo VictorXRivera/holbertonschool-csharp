88ed1b3f110ac411b41e88c50357630d13fb916d
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-14 15:25:48 -0500

    Safe list print program using try/catch

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int h = 0;
+        if (myList.Count == 0)
+            return h;
+
+        try
+        {
+            for (h = 0; h < n; h++)
+                Console.WriteLine(myList[h]);
+        }
+        catch (System.ArgumentOutOfRangeException) { }
+
+        return h;
+    }
+}
