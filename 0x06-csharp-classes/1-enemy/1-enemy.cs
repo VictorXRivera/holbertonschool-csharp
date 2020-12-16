d51c9bda0d8281c5cb83cbb6403235660173dec9
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-15 19:08:16 -0500

    Task 1 files

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Defining public class Zombie
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Variable for health
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Zombie constructor
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
