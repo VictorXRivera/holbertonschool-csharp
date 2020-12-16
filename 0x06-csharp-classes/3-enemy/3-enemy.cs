5ca6c8f22c4487d39e0fdd2e4eae9554ded2fc1d
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-15 20:15:29 -0500

    Added new public method that returns health of zombie

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Defining public class Zombie
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Variable for health
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Zombie constructor
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Constructor with parameter
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+
+        /// <summary>
+        /// New public method
+        /// </summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
