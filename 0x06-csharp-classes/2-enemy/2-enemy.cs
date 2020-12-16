c55a7d7822b77f9028855ec6b6e2f7eaccc1e72a
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-15 20:05:33 -0500

    Task 2 files

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Defining public class Zombie
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Variable for health
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Zombie constructor
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+    }
+}
