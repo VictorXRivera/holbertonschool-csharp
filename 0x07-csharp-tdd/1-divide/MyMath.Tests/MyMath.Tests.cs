651a149a5b40f11d1ae82c69c470fe74cdff02dc
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-19 17:13:18 -0500

    Added more tests

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -10,15 +10,7 @@ namespace MyMath.Tests
             {0, 2, 10},
             {4, 8, 20}
         };
-
-        [Test]
-        public void test_division_of_matrix()
-        {
-            int[,] result = MyMath.Matrix.Divide(_test_matrix, 1);
-            Assert.AreEqual(result, _test_matrix);
-            Assert.AreNotEqual(result, _test_matrix);
-        }
-
+        
         [Test]
         public void test_division_of_matrix_2()
         {
