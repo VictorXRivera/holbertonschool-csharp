6b466c917d9c14e294ce7227fe5860c8e056555b
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-19 17:08:41 -0500

    Added more tests

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -5,18 +5,39 @@ namespace MyMath.Tests
     [TestFixture]
     public class Tests
     {
-        private int[,] test_matrix = new int[,]
+        private int[,] _test_matrix = new int[,]
         {
-            {0, 1, 10},
-            {2, 8, 20}
+            {0, 2, 10},
+            {4, 8, 20}
         };
+
+        [Test]
+        public void test_division_of_matrix()
+        {
+            int[,] result = MyMath.Matrix.Divide(_test_matrix, 1);
+            Assert.AreEqual(result, _test_matrix);
+            Assert.AreNotEqual(result, _test_matrix);
+        }
+
+        [Test]
+        public void test_division_of_matrix_2()
+        {
+            int[,] result = MyMath.Matrix.Divide(_test_matrix, 2);
+            int[,] expected = new int[,]
+            {
+                {0, 1, 5},
+                {2, 4, 10}
+            };
+            Assert.AreEqual(result, expected);
+            Assert.AreNotEqual(result, _test_matrix);
+        }
 
         [Test]
         public void test_division_by_0()
         {
             try
             {
-                int[,] result = MyMath.Matrix.Divide(test_matrix, 0);
+                int[,] result = MyMath.Matrix.Divide(_test_matrix, 0);
                 Assert.AreEqual(result, null);
             }
             catch (System.DivideByZeroException)
