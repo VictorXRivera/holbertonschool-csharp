04522c8ffa9ef5a20eec86445b200936bad8de06
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-20 11:36:08 -0500

    Palindrome method and tests

---
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    [TestFixture]
+    public class Str
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void empty_string()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Text
+{
+    /// <summary> Str class </summary>
+    public class Str
+    {
+        /// <summary> Palindrome method </summary>
+        public static bool IsPalindrome(string s)
+        {
+            s = Regex.Replace(s, "[:;,. \t\n\r]", "");
+            string new_s = new string(s.Reverse().ToArray());
+            if (s == new_s)
+                return true;
+            else
+                return false;
+        }
+    }
+}
