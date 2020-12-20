36be9424d14c6a44b8595effd06f5c3688702f60
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-20 12:21:56 -0500

    Made changes to way method was written

--- 0x07-csharp-tdd/3-palindrome/Text/Text.cs
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -10,8 +10,15 @@ namespace Text
         /// <summary> Palindrome method </summary>
         public static bool IsPalindrome(string s)
         {
-            s = Regex.Replace(s, "[:;,. \t\n\r]", "");
+            if (string.IsNullOrEmpty(s))
+                return true;
+            
+            s = s.ToLower();
+
+            s = Regex.Replace(s, "[^a-zA-Z]", "");
+
             string new_s = new string(s.Reverse().ToArray());
+
             if (s == new_s)
                 return true;
             else
