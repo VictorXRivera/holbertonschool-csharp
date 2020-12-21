9971dfdd33c157ab5e00383e7630185a18e943c8
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-20 22:52:32 -0500

    Task 1 method

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary Object class </summary>
+class Obj
+{
+    /// <summary> Method that checks if object is an array type </summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj.GetType().IsArray)
+            return true;
+        return false;
+    }
+}
