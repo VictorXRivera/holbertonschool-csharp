94286456687a3dd7ce4e8f0b4223ebbd3cffd45e
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-20 23:24:00 -0500

    Method to print properties and methods of an object

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+/// <summary> Object class </summary>
+class Obj
+{
+    /// <summary> Method to print objects </summary>
+    public static void Print(object myObj)
+    {
+        Console.WriteLine("{0} Properties:", myObj.GetType().Name);
+        foreach (var info in myObj.GetType().GetProperties())
+        {
+            Console.WriteLine(info.Name);
+        }
+
+        Console.WriteLine("{0} Methods:", myObj.GetType().Name);
+        foreach (var info in myObj.GetType().GetMethods())
+        {
+            Console.WriteLine(info.Name);
+        }
+    }
+}
