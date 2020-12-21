fc0124623e4927f90fb2a7d2e55159a97bb4ce2d
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-20 23:04:00 -0500

    Method checking for only a subclass

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary> Object class </summary>
+class Obj
+{
+    /// <summary> Method to check if object is only a subclass </summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+            return true;
+        return false;
+    }
+}
