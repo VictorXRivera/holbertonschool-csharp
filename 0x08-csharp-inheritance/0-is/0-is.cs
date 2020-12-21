07bbbdc4b076c7b7120e1c32f74cd6e559780e20
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-20 22:36:27 -0500

    method that returns True if the object is an int, otherwise return False

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+    /// <summary> Object class </summary>
+    class Obj
+    {
+        /// <summary> Method checking if the object is an int </summary>
+        public static bool IsOfTypeInt(object obj)
+        {
+            if (obj.GetType() == typeof(int))
+                return true;
+            return false;
+        }
+    }
