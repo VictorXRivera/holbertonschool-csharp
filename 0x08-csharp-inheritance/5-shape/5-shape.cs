e111c9b26af2b331b4c5dbee6c1c717fd7f092ee
VictorXRivera <victorxrivera.professional@gmail.com>
2020-12-21 10:36:55 -0500

    Shape class with method displaying a message on exception

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+/// <summary> Shape class </summary>
+class Shape
+{
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
