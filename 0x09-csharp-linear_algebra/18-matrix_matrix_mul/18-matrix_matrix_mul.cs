3cecad66193948822b13de3d1695876958f3c55a
VictorXRivera <victorxrivera.professional@gmail.com>
2021-01-21 18:19:13 -0500

    rewrote method

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -5,21 +5,32 @@
 /// </summary>
 class MatrixMath
 {
+    /// <summary>
+    /// method to multiply methods
+    /// </summary>
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
-        double[,] negative = {{-1}};
-        if (matrix1.Length != matrix2.Length)
-            return negative;
-        if (matrix1.GetLength(0) < 2 || matrix2.GetLength(0) < 2 || matrix1.GetLength(0) > 3 || matrix2.GetLength(0) > 3)
-            return negative;
-        
-        for (int one = 0; one < matrix1.GetLength(0); one++)
+        int matrix1Rows = matrix1.GetLength(0);
+        int matrix1Cols = matrix1.GetLength(1);
+        int matrix2Rows = matrix2.GetLength(0);
+        int matrix2Cols = matrix2.GetLength(1);
+
+        if (matrix1Cols != matrix2Rows)
+            return new double[,] { { -1 } };
+
+        double[,] product = new double[matrix1Rows, matrix2Cols];
+        for (int matrix1_row = 0; matrix1_row < matrix1Rows; matrix1_row++)
         {
-            for (int two = 0; two < matrix2.GetLength(1); two++)
-            {
-                matrix1[one, two] = matrix1[one, two] * matrix2[one, two];
+            for (int matrix2_col = 0; matrix2_col < matrix2Cols; matrix2_col++)
+            {  
+                for (int matrix1_col = 0; matrix1_col < matrix1Cols; matrix1_col++)
+                {
+                    product[matrix1_row, matrix2_col] +=
+                      matrix1[matrix1_row, matrix1_col] *
+                      matrix2[matrix1_col, matrix2_col];
+                }
             }
         }
-        return matrix1;
+        return product;
     }
 }
