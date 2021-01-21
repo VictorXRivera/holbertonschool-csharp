fb4af621af6483f8a82a5cd1576b599253690289
VictorXRivera <victorxrivera.professional@gmail.com>
2021-01-21 13:36:42 -0500

    Adding new directory and vector/scalar multiply method

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+/// <summary>
+/// Vector math class
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Method that multiples vector by scalar and returns new vector
+    /// </summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] negative_one = {-1};
+        if (vector.Length < 2 || vector.Length > 3)
+        {
+            return negative_one;
+        }
+        for (int v = 0; v < vector.Length; v++)
+        {
+            vector[v]= vector[v] * scalar;
+        }
+        return vector;
+    }
+}
