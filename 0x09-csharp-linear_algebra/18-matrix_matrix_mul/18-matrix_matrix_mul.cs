6b36158929438a72567712f8b009bb6dd23f0a06
VictorXRivera <victorxrivera.professional@gmail.com>
2021-01-21 17:34:54 -0500

    Multiply matrices method

---
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/// <summary>
+/// Matrix math class
+/// </summary>
+class MatrixMath
+{
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        double[,] negative = {{-1}};
+        if (matrix1.Length != matrix2.Length)
+            return negative;
+        if (matrix1.GetLength(0) < 2 || matrix2.GetLength(0) < 2 || matrix1.GetLength(0) > 3 || matrix2.GetLength(0) > 3)
+            return negative;
+        
+        for (int one = 0; one < matrix1.GetLength(0); one++)
+        {
+            for (int two = 0; two < matrix2.GetLength(1); two++)
+            {
+                matrix1[one, two] = matrix1[one, two] * matrix2[one, two];
+            }
+        }
+        return matrix1;
+    }
+}
