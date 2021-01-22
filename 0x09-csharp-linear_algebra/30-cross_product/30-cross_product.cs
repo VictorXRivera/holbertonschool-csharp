0a63203a2cb80b892ba6045dcb32f1033e39baaf
VictorXRivera <victorxrivera.professional@gmail.com>
2021-01-21 22:13:54 -0500

    Cross product method

---
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary> vector math class </summary>
+class VectorMath
+{
+    /// <summary> 3d vector cross product method </summary>
+    public static double[] CrossProduct(double[] vector1, double[] vector2)
+    {
+         if (vector1.Length == 3 && vector2.Length == 3)
+        {
+            double[] product = new double[vector1.Length];
+            product[0] = (vector1[1] * vector2[2]) - (vector1[2] * vector2[1]);
+            product[1] = (vector1[2] * vector2[0]) - (vector1[0] * vector2[2]);
+            product[2] = (vector1[0] * vector2[1]) - (vector1[1] * vector2[0]);
+            return product;
+        }
+        return new double[] { -1 };
+    }
+}
