d16c5f4064a9d211a52d038064ab3be4bafa6cd7
VictorXRivera <victorxrivera.professional@gmail.com>
2022-04-15 19:53:49 -0400

    Set executable bits

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,1 +1,2 @@
-﻿Console.WriteLine("\"Programming is like building a multilingual puzzle");
+﻿// Used forward slash [ \ ] to omit extra quotation mark
+Console.WriteLine("\"Programming is like building a multilingual puzzle");
