9b23011ba0db12c457d37042c0376b87048b70b2
VictorXRivera <victorxrivera.professional@gmail.com>
2022-04-16 12:49:45 -0400

    C# Program printing number combinations

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 0;
+        while(number <= 98)
+        {
+            Console.Write("{0:00}, ", number);
+            number++;
+        }
+        if (number == 99)
+        {
+            Console.Write("{0}\n", number);
+        }
+    }
+}
