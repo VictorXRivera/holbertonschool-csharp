264ce154e898e0fd47e7a08cd1c10d0101c3120d
VictorXRivera <victorxrivera.professional@gmail.com>
2022-04-15 20:53:15 -0400

    C# Program to print string

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.WriteLine("{0}" + "{0}" + "{0}", str);
+        string halfstr = "Holberton";
+        Console.WriteLine("{0}", halfstr);
+        }
+}
