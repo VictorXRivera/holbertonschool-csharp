b631219c01b6668d88d8e2b67a18f9bac5b143ed
VictorXRivera <victorxrivera.professional@gmail.com>
2022-04-16 13:10:32 -0400

    C# Program printing smallest combinations of two numbers

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int num = 0; num <=99; num++)
+        {
+            int firstdigit = num % 10;
+            int seconddigit = num / 10;
+            if(firstdigit != seconddigit && firstdigit > seconddigit)
+            {
+                if(num != 89)
+                Console.Write("{0:00}, ", num);
+                else
+                Console.WriteLine("{0}", num);
+            }
+        }
+    }
+}
