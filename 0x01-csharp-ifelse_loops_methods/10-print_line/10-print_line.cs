a800223de79e78da86958232c21d01422a80562e
VictorXRivera <victorxrivera.professional@gmail.com>
2022-04-16 14:01:11 -0400

    C# Method printing lines

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,13 @@
+﻿class Line
+{
+    public static void PrintLine(int length)
+    {
+        int iterator = 0;
+        while (iterator < length)
+        {
+            Console.Write('_');
+            iterator++;
+        }
+        Console.WriteLine();
+    }
+}
