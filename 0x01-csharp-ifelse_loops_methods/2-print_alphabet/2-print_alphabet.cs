a83bd02d4245ad9541f7c6605fe6eb1990e4e24f
VictorXRivera <victorxrivera.professional@gmail.com>
2022-04-16 11:16:23 -0400

    C# Program that prints alphabet

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char letter = 'a';
+        while (letter <= 'z')
+        {
+            Console.Write("{0}", letter);
+            letter++;
+        }
+    }
+}
