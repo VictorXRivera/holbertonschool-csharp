634f2dd0fb5f599df16561e43fc2711f270292cd
VictorXRivera <victorxrivera.professional@gmail.com>
2022-04-16 14:13:19 -0400

    C# Method printing diagonal lines

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,16 @@
+﻿class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        
+        for(int iterator1 = 0; iterator1 < length; iterator1++)
+        {
+            for (int iterator2 = 0; iterator2 < iterator1; iterator2++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("\\");
+        }
+        Console.WriteLine("");
+    }
+}
