b1312fddeeece33d70e0a89b99e9453d744ce4da
VictorXRivera <victorxrivera.professional@gmail.com>
2022-04-16 21:00:41 -0400

    C# Method that prints array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,34 @@
+﻿class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size == 0)
+        {
+            Console.WriteLine();
+        }
+        
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] array1 = new int[size];
+
+        for (int num = 0; num < size; num++)
+        {
+            array1[num] = num;
+            Console.Write(array1[num]);
+
+            if (num == size - 1)
+            {
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+        }
+        return (array1);
+    }
+}
