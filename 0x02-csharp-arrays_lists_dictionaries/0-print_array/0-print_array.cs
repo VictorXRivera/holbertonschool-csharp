916d8f6bd33d6ce5a5dabb1f9e11886e2a45659e
VictorXRivera <victorxrivera.professional@gmail.com>
2022-04-16 22:21:19 -0400

    Made changes

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -3,16 +3,24 @@
     public static int[] CreatePrint(int size)
     {
         if (size < 0)
-            {
-                Console.WriteLine("Size cannot be negative");
-                return null;
-            }
-            int[] array1 = new int[size];
-            for(int num = 0; num < size; num++)
-            {
-                array1[num] = num;
-            }
-            Console.WriteLine(String.Join(" ", array1));
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] array1 = new int[size];
+
+        if (size == 0)
+        {
+            Console.WriteLine();
             return array1;
+        }
+        
+        for (int num = 0; num < size; num++)
+        {
+            array1[num] = num;
+        }
+        Console.WriteLine(String.Join(" ", array1));
+        return array1;
     }
 }
