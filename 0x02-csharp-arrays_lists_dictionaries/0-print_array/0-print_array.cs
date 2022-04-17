e1b56eb0a74adb32499651fb9f652e19c5b4615a
VictorXRivera <victorxrivera.professional@gmail.com>
2022-04-16 21:55:28 -0400

    Take three

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -3,23 +3,16 @@
     public static int[] CreatePrint(int size)
     {
         if (size < 0)
-        {
-            Console.WriteLine("Size cannot be negative");
-            return (null);
-        }
-
-        int[] array1 = new int[size];
-
-        for (int num = 0; num < size; num++)
-            array1[num] = num;
-        
-        foreach (var element in array1)
-        {
-            Console.Write(element);
-            if (element != array1[array1.Length - 1])
-                Console.Write(" ");
-        }
-        Console.WriteLine();
-        return array1;
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+            int[] array1 = new int[size];
+            for(int num = 0; num < size; num++)
+            {
+                array1[num] = num;
+            }
+            Console.WriteLine(String.Join(" ", array1));
+            return array1;
     }
 }
