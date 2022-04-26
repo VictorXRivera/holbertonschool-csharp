47b484b9aae440bb1653d6079544e343975a6331
VictorXRivera <victorxrivera.professional@gmail.com>
2022-04-26 09:16:49 -0400

    Method that prints safely

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        for (int element = 0; element < n; element++)
+        {
+            try
+            {
+                Console.WriteLine(myList[element]);
+            }
+
+            catch (ArgumentOutOfRangeException)
+            {
+                return (element);
+            }
+        }
+        return(n);
+    }
+}
