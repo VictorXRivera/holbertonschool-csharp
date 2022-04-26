52bfa196dbbcf917b5b68e1e7adb58d21f5412e0
VictorXRivera <victorxrivera.professional@gmail.com>
2022-04-26 10:07:49 -0400

    Function that divides two integers and prints results

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int divided = 0;
+        try
+        {
+            divided = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine("{0} / {1} = {2}", a, b, divided);
+        }
+    }
+}
