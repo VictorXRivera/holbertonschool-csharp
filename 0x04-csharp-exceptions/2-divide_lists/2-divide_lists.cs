334f0f8af311918fedbb0ae94bd09ec7df369d75
VictorXRivera <victorxrivera.professional@gmail.com>
2022-04-26 10:31:47 -0400

    Function that divides element by element

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> new_list = new List<int>();
+
+        for (int n = 0; n < listLength; n++)
+        {
+            try
+            {
+                new_list.Add(list1[n] / list2[n]);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                new_list.Add(0);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
+        return new_list;
+    }
+}
