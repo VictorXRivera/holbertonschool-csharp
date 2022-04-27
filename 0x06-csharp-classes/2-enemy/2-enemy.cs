ab6b0ed31956612fec287680d4b560331ae659a0
VictorXRivera <victorxrivera.professional@gmail.com>
2022-04-27 12:48:27 -0400

    Added another public constructor

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Public class Zombie defining zombie within the Enemies namespace</summary>///
+    public class Zombie
+    {
+        ///<summary>Public constructor</summary>///
+        public Zombie()
+        {
+            this.health = 0;
+        }
+        ///<summary>Public constructor checking if value is equal or greater than 0</summary>///
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value;
+        }
+        ///<summary>public int field</summary>///
+        public int health;
+    }
+}
