b00e0f0c4eeb1cd2773cb0487eec2f25acdb2690
VictorXRivera <victorxrivera.professional@gmail.com>
2022-04-27 13:16:42 -0400

    Added public property and new edge cases

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Public class Zombie defining zombie within the Enemies namespace</summary>///
+    public class Zombie
+    {
+        ///<summary>Private int field</summary>///
+        private int health;
+        ///<summary>Private string field</summary>///
+        private string name = "(No name)";
+        ///<summary>Public property</summary>///
+        public string Name
+        {
+            get {return name;}
+            set {name = value;}
+        }
+        ///<summary>Public constructor</summary>///
+        public Zombie()
+        {
+            this.health = 0;
+        }
+        ///<summary>Public constructor checking if value is equal or greater than 0</summary>///
+        public Zombie(int? value = 0)
+        {
+            if (value < 0 || value.HasValue == false)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value.GetValueOrDefault();
+        }
+        ///<summary>Method that returns health of Zombie</summary>
+        public int GetHealth()
+        {
+            return (health);
+        }
+    }
+}
