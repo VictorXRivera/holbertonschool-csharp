e9ee1507af53434d9748b623d15a739410b93c7c
VictorXRivera <victorxrivera.professional@gmail.com>
2022-05-01 19:57:50 -0400

    Method that prints list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> new_list = new List<int>();
+
+        for (int n = 0; n < size; n++)
+        {
+            new_list.Add(n);
+            if (n < size - 1)
+            {
+                Console.Write(new_list[n] + " ");
+            }
+            else
+            {
+                Console.Write(new_list[n]);
+            }
+        }
+        Console.WriteLine("");
+        return (new_list);
+    }
+}
