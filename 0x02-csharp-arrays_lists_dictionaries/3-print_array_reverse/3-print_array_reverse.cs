40424949c60c5fcd948a33636f60b9fe62c60c05
VictorXRivera <victorxrivera.professional@gmail.com>
2022-05-01 19:47:33 -0400

    Method that prints array in reverse

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array is null)
+        {
+            Console.WriteLine();
+            return;
+        }
+        for (int a = array.Length - 1; a >= 0; a--)
+        {
+            if (a != array.Length - 1)
+                Console.Write(" ");
+            Console.Write("{0}", array[a]);
+        }
+        Console.WriteLine();
+    }
+}
