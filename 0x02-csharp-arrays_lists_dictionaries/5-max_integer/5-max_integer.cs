7c71c491cb01d445aa0eb1c5d36f890db02c2d85
VictorXRivera <victorxrivera.professional@gmail.com>
2022-05-01 20:14:22 -0400

    Method that finds max integer

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count == null)
+        {
+            Console.WriteLine("List is empty");
+            return (-1);
+        }
+
+        int max_integer = myList[0];
+        foreach (int element in myList)
+        {
+            if (element > max_integer)
+                max_integer = element;
+        }
+        return max_integer;
+    }
+}
