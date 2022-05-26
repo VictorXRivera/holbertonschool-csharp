a222ed714456ccdfc56f082027069154d7760bf4
VictorXRivera <victorxrivera.professional@gmail.com>
2022-05-26 13:43:22 -0400

    Matrix 2D shear method

---
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,33 @@
+﻿using System;
+///<summary>MatrixMath class</summary>
+class MatrixMath
+{
+    ///<summary>Shear2D Method</summary>
+    ///<return>Resulting matrix or -1</return>
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return new double[,] { { -1 } };
+
+
+        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        if (direction == 'x')
+        {
+            result[0, 0] = Math.Round(matrix[0, 0] + factor * matrix[0, 1], 2);
+            result[1, 0] = Math.Round(matrix[1, 0] + factor * matrix[1, 1], 2);
+            result[0, 1] = Math.Round(matrix[0, 1], 2);
+            result[1, 1] = Math.Round(matrix[1, 1], 2);
+            return (result);
+        }
+        else if (direction == 'y')
+        {
+            result[0, 0] = Math.Round(matrix[0, 0], 2);
+            result[1, 0] = Math.Round(matrix[1, 0], 2);
+            result[0, 1] = Math.Round(matrix[0, 1] + factor * matrix[1, 1], 2);
+            result[1, 1] = Math.Round(matrix[1, 1] + factor * matrix[0, 1], 2);
+            return (result);
+        }
+        else
+            return (new double[,] { { -1 } });
+    }
+}
