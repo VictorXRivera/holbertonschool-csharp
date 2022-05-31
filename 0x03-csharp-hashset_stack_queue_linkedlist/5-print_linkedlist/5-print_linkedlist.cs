712ab59baf074f5cfc7ca39dda00f37ffde4608e
VictorXRivera <victorxrivera.professional@gmail.com>
2022-05-31 19:33:56 -0400

    printing linkedlist

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> new_list = new LinkedList<int>();
+
+        if (size < 0)
+        {
+            return (new_list);
+        }
+
+        for (int element = 0; element < size; element++)
+        {
+            Console.WriteLine(element);
+            new_list.AddLast(element);
+        }
+        
+        return (new_list);
+    }
+}
