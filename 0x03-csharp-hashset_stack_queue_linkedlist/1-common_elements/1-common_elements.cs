9ee34efc207aec02ceb38f443bf27d07b0ef6543
VictorXRivera <victorxrivera.professional@gmail.com>
2022-05-31 15:51:45 -0400

    Common elements

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> new_list = new List<int>();
+        for (int c = 0; c < list1.Count; c++)
+        {
+            for (int n = 0; n < list2.Count; n++)
+            {
+                if (list1[c] == list2[n])
+                {
+                    new_list.Add(list1[c]);
+                }
+            }
+        }
+        return new_list;
+    }
+}
