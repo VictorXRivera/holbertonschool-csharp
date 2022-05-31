c116249b0416ed54efa0bbc05a7bd015bb362ca0
VictorXRivera <victorxrivera.professional@gmail.com>
2022-05-31 16:35:25 -0400

    Different elements

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> new_list = list1.FindAll(item => !list2.Contains(item));
+        List<int> compared_list = list2.FindAll(item => !list1.Contains(item));
+        new_list.AddRange(compared_list);
+        new_list.Sort();
+        return(new_list);
+    }
+}
