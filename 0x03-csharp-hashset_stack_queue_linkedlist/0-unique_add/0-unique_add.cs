382b5c3887d901693c7073a8ea201bf1f04d5317
VictorXRivera <victorxrivera.professional@gmail.com>
2022-05-31 13:50:41 -0400

    0x03 Directory

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        var unique = myList.Distinct();
+
+        int sum = 0;
+
+        foreach (int num in unique)
+        {
+            sum += num;
+        }
+
+        return (sum);
+    }
+}
