a6830f461012c4aea973bec96ecc318a4c6a2bd3
VictorXRivera <victorxrivera.professional@gmail.com>
2022-07-26 14:19:30 -0400

    Readded folder

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+///<summary> Queue </summary>///
+public class Queue<T>
+{
+    ///<summary>Method that returns type</summary>
+    public Type CheckType()
+    {
+        return(typeof(T));
+    }
+
+    /// <summary>Class Node inside queue class</summary>
+	public class Node
+	{
+		private T value;
+		public Node next = null;
+
+		public Node(T stri)
+		{
+			value = stri;
+		}
+	}
+
+	public Node head;
+	public Node tail;
+	public int count;
+
+	///<summary>Add new node at the end</summary>
+	public void Enqueue(T value)
+	{
+		Node newnode = new Node(value);
+		if (head == null)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail.next = newnode;
+			tail = newnode;
+		}
+		count++;
+	}
+
+	///<summary>Add new node at the end</summary>
+	public int Count()
+	{
+		return count;
+	}
+
+}
