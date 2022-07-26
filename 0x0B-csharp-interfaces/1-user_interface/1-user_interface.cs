d81908b978ae13adda052f06b71dde7a40915ad4
VictorXRivera <victorxrivera.professional@gmail.com>
2022-07-26 16:15:39 -0400

    Initial commit

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+///<summary>Abstract class</summary>///
+public abstract class Base
+{
+    ///<summary>Public object</summary>///
+    public string name {get; set;}
+    ///<summary>ToString override</summary>///
+    public override string ToString()
+    {
+        return string.Format("{0} is a {1}", name, this.GetType().Name);
+    }
+}
+
+/// <summary>
+/// Make object intractable
+/// </summary>
+interface IInteractive
+{
+    void Interact();
+}
+
+/// <summary>
+/// give objects a durability property and breakable state
+/// </summary>
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+/// <summary>
+/// make an object collectible.
+/// </summary>
+interface ICollectable
+{
+    bool isCollected { get; set; }
+
+    void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public void Interact()
+    {
+    }
+
+
+    public void Break()
+    {
+    }
+
+    public void Collect()
+    {
+    }
+}
