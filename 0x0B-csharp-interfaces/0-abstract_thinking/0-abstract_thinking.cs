9fc1420301dd9b6ce08efd067f281429c9d41c14
VictorXRivera <victorxrivera.professional@gmail.com>
2022-07-26 14:45:29 -0400

    Initial commit

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+///<summary>Abstract class</summary>///
+public abstract class Base
+{
+    ///<summary>Public object</summary>///
+    public string name {get; set;}
+    ///<summary>ToString override</summary>///
+    public override string ToString()
+    {
+        return string.Format("{0} is a {1}", name, this.GetType().Name);
+    }
+}
